7fa5230f7afd7e741bc1b6062d65cd67b66d1d2f
kiernan.victor <kiernan.victor@SEMS-259-06.SEMS259.LAB>
2018-02-28 14:52:58 -0800

    robot- done crqazy

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,16 +5,22 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String cat= JOptionPane.showInputDialog("How many cats do you have");
 		// 2. Convert their answer into an int
-
+int numcat = Integer.parseInt(cat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if (numcat>= 3){
+	JOptionPane.showMessageDialog(null, "You are a crazy cat person. Meow.");
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+else if (numcat< 3 && numcat> 0){
+	playVideo("https://www.youtube.com/watch?v=maXXc4JGdB4");
+}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
 	}
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,45 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot rob  = new Robot();
+		rob.penDown();
+		rob.miniaturize();
 		//3. Ask the user what color they would like the robot to draw
-		
+		for (int i = 0; i < 10000; i++) {
+		String color=		JOptionPane.showInputDialog("What color should the shape be?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+if (color.equalsIgnoreCase("red")) {
+	rob.setPenColor(255, 0, 0);
+} 
+else if (color.equalsIgnoreCase("green")) {
+	rob.setPenColor(0, 255, 0);
+} else if (color.equalsIgnoreCase("blue")) {
+	rob.setPenColor(0, 0, 255);
+} 
+else {
+rob.setRandomPenColor();	
+}
+		
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		;
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		for (int p = 0; p < 360; p++) {
+		rob.move(2);
+rob.turn(1);
+rob.setPenWidth(p);		}
 	}
+}
+
 }
